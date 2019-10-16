61885decf199269c863b793028d3035ca5844fc9
wetcoding <ravil.mashakaev@gmail.com>
2019-10-16 23:26:25 +0500

    FarmService template

--- src/main/java/com/wetcoding/renderfarm/services/FarmService.java
+++ src/main/java/com/wetcoding/renderfarm/services/FarmService.java
@@ -1,4 +1,24 @@
 package com.wetcoding.renderfarm.services;
 
+import com.wetcoding.renderfarm.models.Task;
+
+import java.util.List;
+
 public class FarmService {
+
+    public int login(String email, String password){
+        return 0;
+    }
+
+    public boolean register(String email, String password){
+        return true;
+    }
+
+    public boolean addTask(int userId, String taskName){
+        return true;
+    }
+
+    public List<Task> getTasks(int user_id){
+        return null;
+    }
 }
